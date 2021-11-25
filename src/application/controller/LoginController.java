6faf33404ca5b1e4a3e4bcf2efb5b2c954d977b0
Misael Paxtor <misaelpaxtorh@gmail.com>
2021-11-25 16:45:06 -0600

    Continuation of the Login Page + Processes | User login/sign up handling

--- src/application/controller/LoginController.java
+++ src/application/controller/LoginController.java
@@ -3,6 +3,7 @@ package application.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 import application.Main;
+import application.model.User;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -11,12 +12,22 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class LoginController implements EventHandler<ActionEvent>, Initializable {
 	
-	@FXML Button loginPromptButton, signupPromptButton;
+	@FXML Button signupPromptButton, signupPromptReturnButton, signupPromptSignupButton;
+	@FXML Button loginPromptReturnButton, loginPromptLoginButton, loginPromptButton;
+	@FXML Label loginErrorLabel, signupErrorLabel;
 	@FXML AnchorPane mainPage, loginPrompt, signupPrompt;
+	@FXML TextField loginEmailField, signupEmailField;
+	@FXML PasswordField loginPasswordField, signupMainPasswordField, signupSubPasswordField;
+	
+	private String inEmail;
+	private String inPassword;
 	
 	@Override
 	public void initialize( URL location, ResourceBundle resources )
@@ -24,15 +35,104 @@ public class LoginController implements EventHandler<ActionEvent>, Initializable
 		mainPage.setVisible(true);
 		signupPrompt.setVisible(false);
 		loginPrompt.setVisible(false);
-		
+		clearLabels();
 	}
 	
 	
 	@Override
 	public void handle(ActionEvent event)
 	{
-		mainPage.setVisible(false);
-		signupPrompt.setVisible(true);
+		if( event.getSource() == loginPromptButton )
+		{
+			mainPage.setVisible(false);
+			loginPrompt.setVisible(true);
+		}
+		else if ( event.getSource() == signupPromptButton )
+		{
+			mainPage.setVisible(false);
+			signupPrompt.setVisible(true);
+		}
+		else if ( event.getSource()  == signupPromptReturnButton )
+		{
+			mainPage.setVisible(true);
+			signupPrompt.setVisible(false);
+		}
+		else if ( event.getSource() == loginPromptReturnButton )
+		{
+			mainPage.setVisible(true);
+			loginPrompt.setVisible(false);
+			clearLabels();
+		}
+		else if ( event.getSource() == loginPromptLoginButton )
+		{
+			if( loginEmailField.getText().equals("") || loginPasswordField.getText().equals("") )
+			{
+				loginErrorLabel.setText("Please complete all the fields.");
+			}
+			else
+			{
+				if ( User.validateUser(loginEmailField.getText(), loginPasswordField.getText(), true) )
+				{
+					try {
+						Parent root = FXMLLoader.load(getClass().getResource("../view/Dashboard.fxml"));
+						Main.stage.setScene(new Scene(root, 800, 800));
+						Main.stage.show();
+						
+					} catch(Exception e) {
+						e.printStackTrace();
+					}
+					
+				}
+				else
+				{
+					clearLabels();
+					loginErrorLabel.setText("Invalid Credentials");
+				
+				}
+			}
+		}
+		else if ( event.getSource() == signupPromptSignupButton )
+		{
+			if( signupEmailField.getText().equals("") || signupMainPasswordField.getText().equals("") || signupSubPasswordField.getText().equals("") )
+			{
+				signupErrorLabel.setText("Please complete all the fields.");
+			}
+			else
+			{
+				if( signupMainPasswordField.getText().equals(signupSubPasswordField.getText()) )
+				{
+					//System.out.println( "Creating a user with the credentials " + signupEmailField.getText() + " " + signupMainPasswordField.getText() + " " + signupSubPasswordField.getText() );
+					mainPage.setVisible(true);
+					signupPrompt.setVisible(false);
+					signupErrorLabel.setText("");
+					if( !User.createUser(signupEmailField.getText(), signupMainPasswordField.getText()) )
+					{
+						signupErrorLabel.setText("User all ready exists");
+					}
+					clearLabels();
+				} 
+				else 
+				{
+					signupErrorLabel.setText("Passwords do not match. Try Again.");
+				}
+			}
+			
+		}
+		
+		
+		//System.out.println(event.getSource());
 	
 	}
+	
+	public void clearLabels()
+	{
+		signupEmailField.setText("");
+		signupMainPasswordField.setText("");
+		signupSubPasswordField.setText("");
+		signupErrorLabel.setText("");
+		loginEmailField.setText("");
+		loginPasswordField.setText("");
+		loginErrorLabel.setText("");
+	}
+	
 }
--- src/application/model/User.java
+++ src/application/model/User.java
@@ -1,6 +1,13 @@
 package application.model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class User {
 
@@ -25,6 +32,96 @@ public class User {
         this.events = new ArrayList<>();
     }
 
+    public static boolean validateUser( String username, String password, boolean setActiveUser )
+    {
+    	File userFile = new File( "data/users.txt");
+    	try {
+			userFile.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	
+    	try
+		{
+			BufferedReader fileReader = new BufferedReader( new FileReader( userFile ) );
+			Scanner input = null;
+			String line = null;
+			boolean userNameFound = false;
+			boolean passwordCorrect = false;
+			while( (line = fileReader.readLine()) != null )
+			{
+				input = new Scanner(line);
+				input.useDelimiter(",");
+				String inAccName = input.next();
+				if( (inAccName.equals(username)) )
+				{
+					userNameFound = true;
+					String inAccPassword = input.next();
+					if( (inAccPassword.equals(password)))
+					{
+						passwordCorrect = true;	
+					}
+						
+					
+				}
+				input.close();
+				System.out.println( line );
+			}
+			
+			fileReader.close();
+			
+			
+			if( setActiveUser )
+			{
+				ACTIVE_USER.setUsername(username);
+				ACTIVE_USER.setPassword(password);
+			}
+			
+			return (userNameFound && passwordCorrect);
+			
+		}
+		catch( IOException e)
+		{
+			System.out.println( "Error: Problem reading/writing with the file");
+		}
+    	
+    	return false;
+    }
+    
+    public static boolean createUser( String username, String password )
+    {
+    	File userFile = new File("data/users.txt");
+    	try 
+    	{
+			userFile.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+    	
+    	
+    	if( User.validateUser(username, password, false) )
+    	{
+    		return false;
+    	}
+    	else
+    	{
+	    	try 
+	    	{
+	    		FileWriter writer = new FileWriter("data/users.txt", true);
+	    		writer.write( username + "," + password + "\n");
+	    		writer.close();
+	    		return true;
+	    	} catch (IOException e) {
+	    		e.printStackTrace();
+	    	}
+	    	
+    	}
+    	
+    	return false;
+    	
+    }
+    
     public String getUsername() {
         return username;
     }
